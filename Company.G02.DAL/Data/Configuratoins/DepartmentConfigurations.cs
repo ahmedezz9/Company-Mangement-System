af6ad88700ae87f69b3c04a397ad1b39a3b96077
D E L L <D E L L@DESKTOP-8Q65RMC>
2026-03-23 18:52:50 +0200

    Create DAL

---
+++ Company.G02.DAL/Data/Configuratoins/DepartmentConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Data.Configuratoins
+{
+    public class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(d => d.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
---
+++ Company.G02.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Company.G02.DAL.Data.Configuratoins;
+using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Data.Contexts
+{
+    public class CompanyDbContext :DbContext
+    {
+        public CompanyDbContext() : base()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
